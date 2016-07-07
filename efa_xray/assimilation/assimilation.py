cdda95dd73c06ee0433a94dfcf4a1d7561054798
lmadaus <madaus.le@gmail.com>
2016-07-07 13:53:54 -0700

    Added basic inflation by variable type OR dimension to Assimilation class

--- efa_xray/assimilation/assimilation.py
+++ efa_xray/assimilation/assimilation.py
@@ -3,6 +3,7 @@ from __future__ import print_function
 import numpy as np
 from copy import deepcopy
 import multiprocessing as mp
+from xarray import DataArray
 
 
 
@@ -11,13 +12,26 @@ class Assimilation():
     This class contains methods for computing obs priors and
     splitting up state
     """
-    def __init__(self, state, obs, nproc=1, verbose=False):
-        # Set prior, post and obs
+    def __init__(self, state, obs,nproc=1, inflation=None, verbose=False):
+        """
+        Set prior, post and obs
+        
+        inflate option can either be None (no inflation), a float (all state variables
+        will be inflated by that amount), a dictionary, or a string file name pointing
+        to an npz file of inflation factors.  Dictionary keys may be
+        variable names with values as the inflation factor for that variable, or
+        dimension names as keys with the values being a numpy array the same length
+        as that dimension, with each element being the inflation factor for a given time.
+
+        """
         self.prior = state
         self.obs = obs
         self.post = deepcopy(state)
         self.verbose = verbose
         self.nproc = nproc
+        self.inflation = inflation
+        self.is_inflated = False
+
 
     def compute_ob_priors(self):
         """
@@ -35,29 +49,112 @@ class Assimilation():
         return means, perts
 
 
+    def inflate_state(self):
+        """
+        Inflate the state as specified
+        """
+        # Check if we're already inflated
+        if self.is_inflated:
+            print("State already inflated.  Skipping additional inflation.")
+            return
+
+        varnames = self.prior.vars()
+        if isinstance(self.inflation, float):
+            if self.verbose: print("Inflating all variables by factor: {:3.2f}".format(self.inflation))
+            # Inflate all variables by this number
+            for v in varnames:
+                if self.verbose: print(v, "BEFORE stdev:", np.mean(np.std(self.prior.variables[v], axis=-1), axis=None).values)
+                self.prior.variables[v][:] = self.prior.ensemble_perts().variables[v] * self.inflation + \
+                        self.prior.ensemble_mean().variables[v]
+                if self.verbose: print(v, "AFTER stdev:", np.mean(np.std(self.prior.variables[v], axis=-1), axis=None).values)
+
+        elif isinstance(self.inflation, str):
+            if self.verbose: print("Trying to load inflation from file: {:s}".format(self.inflation))
+            # Load the npy state vector inflation
+            with np.load(self.inflation) as infile:
+                Nstate_inflation = infile.shape[0]
+                if self.prior.Nstate != Nstate_inflation:
+                    # The loaded array is a different size than the state
+                    print("ERROR: the array in the specified inflation file {:s} is of size {:d}, but the size of the prior state vector is {:d}! Not performing inflation.".format(self.inflation, Nstate_inflation, self.prior.Nstate))
+                    return None
+                # Convert the state to a flat vector
+                curstate = self.prior.to_vect()
+                # Update to inflated values
+                curstate = (curstate - curstate.mean(axis=1)[:,None]) * infile + curstate.mean(axis=1)[:,None]
+                # Replace the prior state with the inflated state
+                self.prior.from_vect(curstate)
+                if self.verbose: print("Succeeded inflation from file: {:s}".format(self.inflation))
+        else:
+            # Assume this is a dictionary, so try it as such
+            for k, v in self.inflation.items():
+                if k in ['validtime','lat','lon','x','y']:
+                    if self.verbose: print("Inflating all variables along {:s} dimension".format(k))
+                    # This is a dimension. Check to be sure that the inflation size matches the
+                    # dimension length
+                    Ninflate = v.shape[0]
+                    assert Ninflate == len(self.prior.coords[k])
+                    # Make the numpy array into a data array
+                    thisInflate = DataArray(v, [(k, self.prior.coords[k].values)])
+                    # Get the first variable
+                    if self.verbose: 
+                        priorvars = np.mean(np.mean(np.std(self.prior.variables[varnames[0]],axis=-1), axis=-1),axis=1)
+                        print(varnames[0], "BEFORE stdev:", priorvars)
+                    # Now multiply these together---xarray should handle broadcasting based on array dimensions
+                    self.prior = self.prior.ensemble_perts() * thisInflate + self.prior.ensemble_mean()
+                    if self.verbose: 
+                        aftervars = np.mean(np.mean(np.std(self.prior.variables[varnames[0]],axis=-1), axis=-1),axis=-1)
+                        print(varnames[0], "AFTER stdev:", aftervars)
+                        print(aftervars / priorvars)
+
+
+                else:
+                    # This must be a variable name.  Inflate that variable by the
+                    # specified value
+                    assert isinstance(v, float)
+                    if k not in self.prior.variables.keys():
+                        print("Unable to find variable {:s} to inflate.  Skipping...")
+                        continue
+                    if self.verbose: print("Inflating variable {:s} by factor: {:3.2f}".format(k, v))
+                    if self.verbose: print(k, "BEFORE stdev:", np.mean(np.std(self.prior.variables[k], axis=-1), axis=None).values)
+                    # Update the perturbations for this variable accordingly
+                    self.prior.variables[k][:] = self.prior.ensemble_perts().variables[k] * v + self.prior.ensemble_mean().variables[k]
+                    if self.verbose: print(k, "AFTER stdev:", np.mean(np.std(self.prior.variables[k], axis=-1), axis=None).values)
+                
+
+        # Set the flag to inflated
+        self.is_inflated = True
+
     def format_prior_state(self):
         """
         Builds state into vector
-        Splits the state into chunks for potential multiprocessing
         """
-        # Get ob priors first
-        if self.verbose: print("Computing observation priors")
-        obmeans, obperts = self.compute_ob_priors()
         Nens = self.prior.nmems()   # Number of ensemble members
         Nstate = self.prior.nstate()   # Number of variables in state vector
         Nobs = len(self.obs)     # Total number of observations
+        # Get the full state shape
+        statesize = self.prior.shape()
         
+        
+        # Option here to inflate state
+        if self.inflation is not None:
+            if self.verbose: print("Inflating Prior State")
+            self.inflate_state()
+                
+        # Get ob priors
+        if self.verbose: print("Computing observation priors")
+        obmeans, obperts = self.compute_ob_priors()
+
         # Convert state to vector
         if self.verbose: print("Converting state to vector")
         prior = self.prior.to_vect()
-        
+
         # Divide based on how many processors we need
-        if self.nproc <= 1:
-            xbm = prior.mean(axis=1)
-            Xbp = prior - xbm[:,None]
-            # Compute observation priors and append to state
-            xbm = np.hstack((xbm, obmeans))
-            Xbp = np.vstack((Xbp, obperts))
+        #if self.nproc <= 1:
+        xbm = prior.mean(axis=1)
+        Xbp = prior - xbm[:,None]
+        # Compute observation priors and append to state
+        xbm = np.hstack((xbm, obmeans))
+        Xbp = np.vstack((Xbp, obperts))
         # Need other case here for multiprocessing
         
         
